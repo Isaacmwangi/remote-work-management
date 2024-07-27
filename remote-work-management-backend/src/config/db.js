8bab2a18210586de3f542918bbdf0a92370281be
Isaacmwangi <njugisaac@gmail.com>
2024-07-27 11:34:45 +0300

    Fix node_modules conflict issue

--- remote-work-management-backend/src/config/db.js
+++ remote-work-management-backend/src/config/db.js
@@ -1,14 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const connectDB = () => {
-  prisma.$connect()
-    .then(() => {
-      console.log('Connected to the database');
-    })
-    .catch((error) => {
-      console.error('Error connecting to the database', error);
-    });
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+  }
 };
 
 module.exports = { prisma, connectDB };
